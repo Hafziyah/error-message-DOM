d13ad56afe026ce4f0b375b45eaa7e867be19171
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-02-09 16:54:20 +0100

    Scrimba tutorial -using jScript to make error msg

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let errorMessage=document.getElementById("error")
+console.log(errorMessage)
+
+function purchase(){
+    console.log("button clicked")
+    errorMessage.textContent="Something went wrong, try again"
+}
+
